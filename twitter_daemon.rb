efd4d5182ff5103dad651ccca901de51c97c82f2
Taiki ONO <taiks.4559@gmail.com>
2013-03-10 00:42:28 +0900

    wip: inplement with DataStore

--- twitter_daemon.rb
+++ twitter_daemon.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'yaml'
 require 'twitter'
+require 'mongo'
 
 $LOAD_PATH.unshift File.expand_path('../lib/chatroid/lib', __FILE__)
 require 'chatroid'
@@ -18,10 +19,6 @@ module TwitterDaemon
     end
 
     def run!
-      Signal.trap :INT do
-        puts "\nexitting.."
-        exit
-      end
       puts 'starting...'
       Client.new.register!.run!
     end
@@ -34,6 +31,10 @@ module TwitterDaemon
       end
     end
 
+    def store
+      @store ||= DataStore.new
+    end
+
     def register!
       methods.each do |name|
         callbacks[$1] << method(name).to_proc if name =~ /^on_(?!time)(.+)$/
@@ -42,14 +43,65 @@ module TwitterDaemon
     end
 
     def on_tweet(event)
+      store.save event
     end
 
     def on_favorite(event)
-      p event["target"]
+      store.favorite event["target_object"]["id"]
+      p 'favorited'
+      p event["target_object"]["id"]
     end
 
     def on_favorite_other(event)
-      p event["target"]
+      store.favorite event["target_object"]["id"]
+      p 'favorited other'
+      p event["target_object"]["id"]
+    end
+
+    def on_unfavorite(event)
+      store.unfavorite event["target_object"]["id"]
+      p 'unfavorited'
+      p event["target_object"]["id"]
+    end
+
+    def on_unfavorite_other(event)
+      store.unfavorite event["target_object"]["id"]
+      p 'unfavorited other'
+      p event["target_object"]["id"]
+    end
+  end
+
+  class DataStore
+    def client
+      @client ||= Mongo::MongoClient.new
+    end
+
+    def db
+      @db ||= client.db('tweeamon')
+    end
+
+    def collection
+      @collection ||= db.collection('tweets')
+    end
+
+    def save(obj)
+      collection.insert obj
+    end
+
+    def favorite_count_of(id)
+      collection.find({"id" => id}).first["favorite_count"]
+    end
+
+    def op_of(id)
+      favorite_count_of(id) ? "$inc" : "$set"
+    end
+
+    def favorite(id)
+      collection.update({"id" => id}, {op_of(id) => {"favorite_count" => 1}})
+    end
+
+    def unfavorite(id)
+      collection.update({"id" => id}, {"$inc" => {"favorite_count" => -1}})
     end
   end
 end
