045c65908dcad0aa8ebfe29d040dadba8e30367c
Taiki ONO <taiks.4559@gmail.com>
2013-03-10 18:27:00 +0900

    refactoring: add Document class for hundling results

--- lib/twitter_daemon/data_store.rb
+++ lib/twitter_daemon/data_store.rb
@@ -12,18 +12,39 @@ module TwitterDaemon
       collection.insert obj
     end
 
-    def find_by(id)
-      collection.find({"id" => id}).first
+    def favorite(id, source)
+      doc = Document.new(collection.find({"id" => id}).first)
+      unless doc.nil? or doc.favorited_by(source)
+        collection.update(
+          {"id" => id},
+          {"$inc" => {"favorite_count" => 1}, "$push" => {"favorite_users" => source}}
+        )
+      end
     end
 
-    def favorite(id, source)
-      if tweet = find_by(id)
-        if !(tweet["favorite_users"]) || (tweet["favorite_users"] && !(tweet["favorite_users"].map {|source| source["id"] }.include?(source["id"])))
-          collection.update(
-            {"id" => id},
-            {"$inc" => {"favorite_count" => 1}, "$push" => {"favorite_users" => source}}
-          )
-        end
+    class Document
+      def initialize(raw)
+        @raw = raw
+      end
+
+      def favorited?
+        @raw["favorite_count"] and @raw["favorite_users"]
+      end
+
+      def favorited_by(user)
+        favorited_users.include?(user["id"])
+      end
+
+      def favorited_users
+        favorited? ? @raw["favorite_users"].map { |source| source["id"] } : []
+      end
+
+      def method_missing(name, *args)
+        raw.send(name, *args)
+      end
+
+      def respond_to_missing?(name, include_private)
+        raw.respond_to_missing? name, include_private
       end
     end
   end
