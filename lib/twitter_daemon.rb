b6d78c7fe903960a611fbe42aaf794d645ffdac8
Taiki ONO <taiks.4559@gmail.com>
2013-04-11 23:29:41 +0900

    move codes to lib

--- lib/twitter_daemon.rb
+++ lib/twitter_daemon.rb
@@ -4,4 +4,38 @@ require 'twitter_daemon/client'
 require 'twitter_daemon/data_store'
 
 module TwitterDaemon
+  CLIENT_CONF = %w(
+    service consumer_key consumer_secret access_key access_secret
+  ).map(&:to_sym).freeze
+
+  class << self
+    include Logging
+
+    def config
+      @config ||= Hash[YAML.load_file(path).map { |k, v| [k.to_sym, v] }]
+    end
+
+    def client_conf
+      config.select { |k, v| CLIENT_CONF.include? k }
+    end
+
+    def datastore_conf
+      config.select { |k, v| [:database_name].include? k }
+    end
+
+    def path
+      File.expand_path('../../config.yaml', __FILE__)
+    end
+
+    def run!
+      info 'starting service...'
+      loop do
+        begin
+          Client.new(client_conf).register!.run!
+        rescue => e
+          fatal e
+        end
+      end
+    end
+  end
 end
--- twitter_daemon.rb
+++ twitter_daemon.rb
@@ -10,41 +10,5 @@ $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'chatroid'
 require 'twitter_daemon'
 
-module TwitterDaemon
-  CLIENT_CONF = %w(
-    service consumer_key consumer_secret access_key access_secret
-  ).map(&:to_sym).freeze
-
-  class << self
-    include Logging
-
-    def config
-      @config ||= Hash[YAML.load_file(path).map { |k, v| [k.to_sym, v] }]
-    end
-
-    def client_conf
-      config.select { |k, v| CLIENT_CONF.include? k }
-    end
-
-    def datastore_conf
-      config.select { |k, v| [:database_name].include? k }
-    end
-
-    def path
-      File.expand_path('../config.yaml', __FILE__)
-    end
-
-    def run!
-      info 'starting service...'
-      loop do
-        begin
-          Client.new(client_conf).register!.run!
-        rescue => e
-          fatal e
-        end
-      end
-    end
-  end
-end
 
 TwitterDaemon.run! if $0 == __FILE__
